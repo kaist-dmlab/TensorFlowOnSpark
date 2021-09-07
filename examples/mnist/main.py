fb6567750c114475a3073b460f82ad1649fa1a97
Junhyeok Kang <junhyeok.kang@kaist.ac.kr>
2021-09-07 18:01:40 +0900

    Update main.py

--- examples/mnist/main.py
+++ examples/mnist/main.py
@@ -22,6 +22,7 @@ from examples.mnist.db_manager import DBManager
 
 # from DBManager import DBManager
 class DBManager():
+    
     def __init__(self):
         print("==========Start DBManager==========")
 
